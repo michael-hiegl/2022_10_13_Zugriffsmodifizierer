42ed1d4b6accf5b035afbd1fc1a929ce6efedae8
michael-hiegl <100510019+michael-hiegl@users.noreply.github.com>
2022-10-14 12:13:01 +0200

    Add files via upload

---
+++ 2022_10_13_Zugriffsmodifizierer/2022_10_13_Zugriffsmodifizierer/Program.cs
@@ -0,0 +1,48 @@
+﻿//Imports######################################################
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//#############################################################
+
+//Classes######################################################
+class Auto
+{
+    private int Reifen;
+    private double Motor;
+    private string Türen;
+
+    public void SetReifen(int Reifengröße)
+    {
+        if (Reifengröße < 15 || Reifengröße > 20)
+        {
+            Reifen = 0;
+        }
+        else
+        {
+            Reifen = Reifengröße;
+        }
+    }
+}
+//#############################################################
+
+namespace _2022_10_13_Zugriffsmodifizierer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Code#############################################
+            Auto meinAuto = new Auto();
+
+            meinAuto.SetReifen(21);
+
+            Console.WriteLine("Zulässige Reifengröße");
+            //#################################################
+
+            Console.WriteLine("Press ENTER to exit...");
+            Console.ReadKey();
+        }
+    }
+}
